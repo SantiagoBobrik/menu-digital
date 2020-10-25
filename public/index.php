ba9d4d1d811d5967d30c4857b92d6e9f07b85776
SantiagoDiaz <santiago.bobrik@gmail.com>
2020-10-24 22:17:20 -0300

    CRUD para restauntes

--- public/index.php
+++ public/index.php
@@ -3,15 +3,17 @@
 use DI\Container;
 use Slim\Factory\AppFactory;
 
+//error_reporting(E_ALL);
+//ini_set('display_errors', true);
+//ini_set('display_startup_errors', true);
+
 require __DIR__ . '/../vendor/autoload.php';
-
-
 #Create Container
 AppFactory::setContainer(new \DI\Container);
 
 #Create App
 $app = AppFactory::create();
-
+$app->addBodyParsingMiddleware();
 #Get Container
 $container = $app->getContainer();
 
@@ -23,5 +25,6 @@ require __DIR__ . '/../src/dependencies.php';
 
 /* ----------------------------- Get Routes ------------------------------------- */
 require __DIR__ . '/../src/App/Routes/productRoutes.php';
+require __DIR__ . '/../src/App/Routes/restaurantRoutes.php';
 
 $app->run();
